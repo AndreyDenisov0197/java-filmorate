3da9b4f300248cf4c88202c2720c683dc513aeb8
AndreyDenisov0197 <denisov0197@yandex.ru>
2023-11-22 23:53:59 +0300

    fix: исправить ошибки

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     void deleteFilm(Film fim);
+
     Film updateFilm(Film film);
+
     List<Film> getFilm();
+
     Film getFilmByID(Integer id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User addUser(User user);
+
     void deleteUser(User user);
+
     List<User> getUser();
+
     User updateUser(User user);
+
     User getUserByID(int id);
 }
